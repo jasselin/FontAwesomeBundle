99b0847d790746c844b770645a25696bb2012580
Jason Asselin <jason.asselin@cdid.com>
2020-04-16 12:09:57 -0400

    Bundle

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faFire } from '@fortawesome/pro-regular-svg-icons'
+import { faAlien } from '@fortawesome/pro-duotone-svg-icons'
+
+library.add(faFire)
+library.add(faAlien)
+
+dom.watch()
